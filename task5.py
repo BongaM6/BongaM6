46d48b22a27cccfa371678b4492a7c9c9319dd6b
BongaM6 <158094257+BongaM6@users.noreply.github.com>
2024-02-15 22:01:33 +0200

    task5.py

---
+++ task5.py
@@ -0,0 +1,12 @@
+num1 = input()
+num2 = input()
+num3 = input()
+
+if (num1 >= num2) and (num1 >= num3):
+   task5 = num1
+elif (num2 >= num1) and (num2 >= num3):
+   task5 = num2
+else:
+   task5 = num3
+print(task5)
+
